1d7178a5a7f1cd25fcb845f757b8f2757fd0d7b2
DawnMoon_Honor <dmoon1542@gmail.com>
2025-10-27 18:52:00 +0800

    (fix) 添加非空检查注解

--- src/main/java/com/dawnmoon/charon/common/security/JwtAuthenticationFilter.java
+++ src/main/java/com/dawnmoon/charon/common/security/JwtAuthenticationFilter.java
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -36,9 +37,9 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private static final String TOKEN_PREFIX_BEARER = "Bearer ";
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request,
-                                  HttpServletResponse response,
-                                  FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
 
         // 1. 从请求头获取 Token
         String token = getTokenFromRequest(request);
