ab80a6ba285d13864e710b873b1f9a9e9632840b
DawnMoon_Honor <dmoon1542@gmail.com>
2025-11-13 10:57:03 +0800

    (test) 添加bcrypt的测试

--- src/test/java/com/dawnmoon/charon/encrypt/BcryptTest.java
+++ src/test/java/com/dawnmoon/charon/encrypt/BcryptTest.java
@@ -9,8 +9,15 @@ public class BcryptTest {
 
     @Test
     public void genBcryptResult() {
-        String rawPassword = "qut@123456";
-        String bcryptResult = CryptoUtil.bcryptEncode(rawPassword);
-        System.out.println("rawPassword=" + rawPassword + "\nbcryptResult=" + bcryptResult);
+        String rawPassword1 = "qut@123456";
+        String rawPassword2 = "qut@123456";
+        String rawPassword3 = "qut@1234567";
+        String bcryptResult1 = CryptoUtil.bcryptEncode(rawPassword1);
+        String bcryptResult2 = CryptoUtil.bcryptEncode(rawPassword2);
+        String bcryptResult3 = CryptoUtil.bcryptEncode(rawPassword3);
+        System.out.println("rawPassword1=" + rawPassword1 + "\nrawPassword2=" + rawPassword2 + "\nrawPassword3=" + rawPassword3);
+        System.out.println("bcryptResult1=" + bcryptResult1 + "\nbcryptResult2=" + bcryptResult2 + "\nbcryptResult3=" + bcryptResult3);
+        System.out.println("pw1 = pw2? " + CryptoUtil.bcryptMatches(rawPassword1, bcryptResult1));
+        System.out.println("pw1 = pw3? " + CryptoUtil.bcryptMatches(rawPassword1, bcryptResult3));
     }
 }
